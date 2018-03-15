4e32bf6301aeadcd1555592d03978262fbea2ddd
davidv <david.vondrak@vanio.cz>
2018-03-15 13:03:05 +0100

    Fix.

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -9,7 +9,6 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
