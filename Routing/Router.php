ec04882f0888e0c771b99e055ac16344b8b3e357
Marek Štípek <mario.dweller@seznam.cz>
2018-05-23 19:20:58 +0200

    Implemented RequestMatcherInterface in decorated Router

--- Routing/Router.php
+++ Routing/Router.php
@@ -1,13 +1,15 @@
 <?php
 namespace Vanio\UserBundle\Routing;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RouterInterface;
 use Vanio\UserBundle\Security\TargetPathResolver;
 
-class Router implements RouterInterface
+class Router implements RouterInterface, RequestMatcherInterface
 {
     /** @var RouterInterface */
     private $router;
@@ -37,6 +39,16 @@ class Router implements RouterInterface
     public function match($pathinfo): array
     {
         return $this->router->match($pathinfo);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function matchRequest(Request $request): array
+    {
+        return $this->router instanceof RequestMatcherInterface
+            ? $this->router->matchRequest($request)
+            : $this->match($request->getPathInfo());
     }
 
     /**
