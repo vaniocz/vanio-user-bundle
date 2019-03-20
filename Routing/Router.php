c4adaf8d5456549389fdb8ee5dc24c369ac300a7
Marek Štípek <mario.dweller@seznam.cz>
2019-03-20 14:46:23 +0100

    Support for generating of routes based on route name prefix request parameter

--- Routing/Router.php
+++ Routing/Router.php
@@ -3,10 +3,12 @@ namespace Vanio\UserBundle\Routing;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Exception\ExceptionInterface;
 use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RouterInterface;
+use Vanio\Stdlib\Strings;
 use Vanio\UserBundle\Security\TargetPathResolver;
 
 class Router implements RouterInterface, RequestMatcherInterface
@@ -16,6 +18,9 @@ class Router implements RouterInterface, RequestMatcherInterface
 
     /** @var TargetPathResolver|null */
     private $targetPathResolver;
+
+    /** @var bool */
+    private $passTargetPath = false;
 
     /** @var RequestStack */
     private $requestStack;
@@ -29,6 +34,11 @@ class Router implements RouterInterface, RequestMatcherInterface
     public function setTargetPathResolver(TargetPathResolver $targetPathResolver): void
     {
         $this->targetPathResolver = $targetPathResolver;
+    }
+
+    public function setPassTargetPath(bool $passTargetPath): void
+    {
+        $this->passTargetPath = $passTargetPath;
     }
 
     /**
@@ -60,11 +70,19 @@ class Router implements RouterInterface, RequestMatcherInterface
      */
     public function generate($name, $parameters = [], $referenceType = self::ABSOLUTE_PATH): string
     {
-        if ($this->targetPathResolver && $name === 'fos_user_security_login') {
-            if ($request = $this->requestStack->getCurrentRequest()) {
+        if ($request = $this->requestStack->getCurrentRequest()) {
+            if ($this->targetPathResolver && $this->passTargetPath && $name === 'fos_user_security_login') {
                 if ($targetPath = $this->targetPathResolver->resolveTargetPath($request)) {
                     $parameters[$this->targetPathResolver->targetPathParameter()] = $targetPath;
                 }
+            }
+
+            $namePrefix = $request->attributes->get('_user_route_name_prefix');
+
+            if ($namePrefix && Strings::startsWith($name, ['fos_user_', 'vanio_user_'])) {
+                try {
+                    return $this->router->generate($namePrefix . $name, $parameters, $referenceType);
+                } catch (ExceptionInterface $e) {}
             }
         }
 
