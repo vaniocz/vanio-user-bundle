bffa4c6dd61ef38218b96cb7232c1fda0167dd26
dvondrak <david.vondrak@ongr.io>
2019-12-18 19:24:20 +0100

    Reload user after profile editAction to prevent changing logged in user when invalid form is submitted. (#21)

--- Controller/ProfileController.php
+++ Controller/ProfileController.php
@@ -2,8 +2,27 @@
 namespace Vanio\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\ProfileController as BaseProfileController;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends BaseProfileController
 {
     use ResponseFormatTrait;
+
+    public function editAction(Request $request): Response
+    {
+        $response = parent::editAction($request);
+
+        if ($user = $this->getUser()) {
+            $this->userManager()->reloadUser($user);
+        }
+
+        return $response;
+    }
+
+    private function userManager(): UserManagerInterface
+    {
+        return $this->get('fos_user.user_manager');
+    }
 }
